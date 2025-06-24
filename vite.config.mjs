2d0d528bb78f08ccea82afa68bd31c715e2f67d1
cheezeburritos <barrientoschaz1983@gmail.com>
2025-06-25 06:08:40 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,8 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // ...your other settings
 })
